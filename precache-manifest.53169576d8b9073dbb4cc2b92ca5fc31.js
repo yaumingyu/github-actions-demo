cd7730eb27cd6f86ae7f10dfde323c39283c608c
yaumingyu <838934617@qq.com>
2020-07-15 03:14:42 +0000

    Deploying to gh-pages from master 9a41358d86295f5499c92d49186ffab3160af500

---
+++ precache-manifest.53169576d8b9073dbb4cc2b92ca5fc31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70905c31f968b507511ab9e8e0727497",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "45f6e2cbffd4246a9670",
+    "url": "/github-actions-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2880fe0a6e61e99907b",
+    "url": "/github-actions-demo/static/js/2.3b5b7970.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/github-actions-demo/static/js/2.3b5b7970.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45f6e2cbffd4246a9670",
+    "url": "/github-actions-demo/static/js/main.0c293af1.chunk.js"
+  },
+  {
+    "revision": "bc9ba4879b37f06ee7dc",
+    "url": "/github-actions-demo/static/js/runtime-main.1c9723b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
